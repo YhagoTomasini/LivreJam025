1d0c9724ee663a5aa329d7dcd1eca1208a09fa98
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-10-04 18:18:24 -0300

    update tileset chao

--- Scripts/golem.gd
+++ Scripts/golem.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 		#print(Globals.podeMover)
 		
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor() or Input.is_action_just_pressed("ui_up") and is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
 			velocity.y = Globals.PULO
 
 		var direction := Input.get_axis("ui_left", "ui_right")
