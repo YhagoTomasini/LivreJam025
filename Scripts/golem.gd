b2d7fd208f354278f6b97429ee4d89679e0bb0e5
Artêmio Neto <artemiorabello123@gmail.com>
2025-10-04 18:51:07 -0300

    Correção do script e teste do tilemap

--- Scripts/golem.gd
+++ Scripts/golem.gd
@@ -34,4 +34,4 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "tiro_totem":
-		get_tree().reload_current_scene()
+		get_tree().call_deferred("reload_current_scene")
