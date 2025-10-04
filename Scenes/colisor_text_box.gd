eec8d62d918fa75baf7ea60019ba4392fb8c6d01
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-10-04 14:46:01 -0300

    update caixa de texto

--- Scenes/colisor_text_box.gd
+++ Scenes/colisor_text_box.gd
@@ -3,18 +3,38 @@ extends Area2D
 var emCima: bool
 var posiMouse := Vector2.ZERO
 var diferenca : Vector2
+@onready var hitboxText : CollisionShape2D = $HitboxTextBox/CollisionShape2D
 
+var semMover: bool
+var podeClick: bool
+
+
+func _ready() -> void:
+	semMover = true
+	podeClick = false
+	
 
 func _on_mouse_entered() -> void:
 	emCima = true
 	
 func _on_mouse_exited() -> void:
 	emCima = false
+		
 	
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	diferenca = posiMouse - get_global_mouse_position()
 	
-	if Input.is_action_pressed("click") and emCima and diferenca != Vector2.ZERO:
-		global_position -= diferenca
+	if emCima:
+		podeClick = true
+	
+	if podeClick:
+		if Input.is_action_pressed("click"):
+			if diferenca != Vector2.ZERO:
+				global_position -= diferenca
+				hitboxText.disabled = true
+			
+		else:
+			hitboxText.disabled = false
+			podeClick = false
 		
 	posiMouse = get_global_mouse_position()
