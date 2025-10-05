ff60b5874469c9a00870114165ea5ac3d5502547
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-10-05 18:31:15 -0300

    botoes

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -22,3 +22,8 @@ func _on_resume_btn_pressed() -> void:
 func _on_bm_btn_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/menu_start.tscn")
+
+
+func _on_reiniciar_button_down() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
