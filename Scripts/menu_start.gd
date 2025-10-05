3ec34569d60513414fbaae712aa545412d34c818
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-10-05 18:58:57 -0300

    menu

--- Scripts/menu_start.gd
+++ Scripts/menu_start.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_play_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_creditos_button_down() -> void:
+	pass # Replace with function body.
