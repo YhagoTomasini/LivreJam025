1b5af9e0d120860afbfd9bed67a2e75d7bdd9004
Yhago Tomasini <yhagooliveira20@gmail.com>
2026-07-17 08:00:07 -0300

    Update vol_slider.gd

--- Scripts/vol_slider.gd
+++ Scripts/vol_slider.gd
@@ -7,5 +7,7 @@ func _ready() -> void:
 	busIndex = AudioServer.get_bus_index(nomeBus)
 	value_changed.connect(volume_alterado)
 	
+	value = db_to_linear(AudioServer.get_bus_volume_db(busIndex))
+	
 func volume_alterado(valor : float) -> void:
 	AudioServer.set_bus_volume_db(busIndex, linear_to_db(valor))
